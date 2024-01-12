a817cae13a05716c9763b43d701668454ee30b95
nadhiya <nadhiyababu321@gmail.com>
2024-01-12 15:06:53 +0530

    video

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React from 'react';
+
+
+
+function App() {
+  
+  return (
+    <div>
+      <h1 className='center'> Video Player</h1>
+    
+    <video className='video' width='600' controls>
+     
+       <source  src="./img/mov_bbb.mp4" type='video/mp4'
+            
+
+      
+  
+      /> 
+
+    </video>
+    </div>
+    
+  );
+}
+
+  export default App;
